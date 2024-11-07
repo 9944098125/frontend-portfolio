a82da0fc9bb6aa68404c569e2e3ed2c780b58b6f
Srinivas Akella <akellasrinivas9944@gmail.com>
2024-11-07 23:06:17 +0530

    updated the api

--- src/Redux/Api/Api.ts
+++ src/Redux/Api/Api.ts
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
 export const Api = Axios.create({
-	baseURL: "https://full-stack-portfolio-odnq.onrender.com/api",
+	// baseURL: "https://full-stack-portfolio-odnq.onrender.com/api",
+	baseURL: "https://full-stack-portfolio-production-8787.up.railway.app/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
